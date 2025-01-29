88ce4a20d4e9ae5bc9647e329082f40a845f7596
unknown <Samarin.K@sh.hub>
2025-01-29 13:33:48 +0300

    add files of project

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_number: int) -> str:
+    card_number = list(str(card_number))
+    for index in range(6, 12):
+        card_number[index] = "*"
+    card_number.insert(4, " ")
+    card_number.insert(9, " ")
+    card_number.insert(14, " ")
+    card_number = "".join(card_number)
+    return card_number
+
+
+def get_mask_account(account_number: int) -> str:
+    return f"**{str(account_number)[-4:]}"
