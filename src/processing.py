a64a2cb0962b03e3c10459d9c3a38af73cb3cf2a
unknown <Samarin.K@SH.HUB>
2025-02-11 12:24:35 +0300

    new function

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,16 @@
 def filter_by_state(dicts, state="EXECUTED"):
+    """Принимает список словарей в параметр dicts, после чего возвращает те словари, у которых значение state совпадает
+    со значением одноименного параметра"""
     dicts_to_return = []
     for elem in dicts:
         if elem["state"] == state:
             dicts_to_return.append(elem)
     return dicts_to_return
 
+def sort_by_date(dicts, order_sort="down"):
+    """Принимает список словарей в параметр dicts, после чего возвращает его, отсортировав по дате. Значение параметра order_sort показывает
+    порядок сортировки: down - по убыванию, up - по возрастанию"""
+    if order_sort=="down":
+        return sorted(dicts, key=lambda elem: elem["date"], reverse=True)
+    if order_sort=="up":
+        return sorted(dicts, key=lambda elem: elem["date"])
