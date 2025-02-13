5c0764c0ffe13475953eb932e9d1bac0741ed7a2
unknown <Samarin.K@sh.hub>
2025-02-13 15:09:10 +0300

    functions in processing clearer

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(dicts, state="EXECUTED"):
+def filter_by_state(dicts: list, state: str="EXECUTED") -> list:
     """Принимает список словарей в параметр dicts, после чего возвращает те словари, у которых значение state совпадает
     со значением одноименного параметра"""
     dicts_to_return = []
@@ -7,7 +7,8 @@ def filter_by_state(dicts, state="EXECUTED"):
             dicts_to_return.append(elem)
     return dicts_to_return
 
-def sort_by_date(dicts, reverse=True):
+
+def sort_by_date(dicts: list, reverse: bool=True) -> list:
     """Принимает список словарей в параметр dicts, после чего возвращает его, отсортировав по дате. Значение параметра
     reverse показывает порядок сортировки: True - по убыванию, False - по возрастанию"""
     if reverse:
