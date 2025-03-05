ec2a5eafc656c3969e894f2499fea7124a44202b
Samarin.K <kiruha2387@gmail.com>
2025-03-05 12:17:36 +0300

    deleted prints for testing functions

--- src/masks.py
+++ src/masks.py
@@ -20,5 +20,3 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счета"""
     return f"**{account_number[-4:]}"
-
-print(get_mask_account(""))
