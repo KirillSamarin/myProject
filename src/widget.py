ff7af47aaf44a1dc4218abaae4df38b0b33f5ab7
Samarin.K <kiruha2387@gmail.com>
2025-03-05 11:36:29 +0300

    adding tests

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,19 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
+from dateutil import parser
 
 def mask_account_card(account_card_number: str) -> str:
     """Принимает номер счета или карты, а затем маскирует"""
     account_card_number = account_card_number.split()
     if "счет" in account_card_number[0].lower():
-        account_card_number[-1] = get_mask_account(int(account_card_number[-1]))
+        account_card_number[-1] = get_mask_account(account_card_number[-1])
     else:
-        account_card_number[-1] = get_mask_card_number(int(account_card_number[-1]))
+        account_card_number[-1] = get_mask_card_number(account_card_number[-1])
     return " ".join(account_card_number)
 
 def get_date(date: str) -> str:
-    """Принимает строку в формате 2024-03-11T02:26:18.671407 и возвращает из нее дату в формате ДД.ММ.ГГГГ(11.03.2024)"""
-    date = date[0:10].split("-")
-    return ".".join(date[::-1])
-
+    """Принимает строку в формате 2024-03-11T02:26:18.671407(часть после T необяазательна) и возвращает из нее дату в формате ДД.ММ.ГГГГ(11.03.2024)"""
+    try:
+        date = parser.parse(date).date()
+        return date.strftime("%d.%m.%Y")
+    except:
+        return "Введите данные в корректном формате"
---
+++ tests/test_widget.py
@@ -0,0 +1,12 @@
+from src.widget import mask_account_card, get_date
+import pytest
+
+@pytest.mark.parametrize("type_number, masked", [("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"), ("Счет 73654108430135874305", "Счет **4305"), ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"), ("7000792289606361", "7000 79** **** 6361")])
+def test_mask_card_account(type_number, masked):
+    assert mask_account_card(type_number) == masked
+
+def test_get_date(date, text):
+    assert get_date(date) == "11.03.2024"
+    assert get_date(text) == "Введите данные в корректном формате"
+    assert get_date("") == "Введите данные в корректном формате"
+    assert get_date("03 11 2024") == "11.03.2024"
