e64dc59d8c75ed91cda08324ba33816795c4660c
Samarin.K <kiruha2387@gmail.com>
2025-03-06 09:36:06 +0300

    adding docstrings

--- src/generators.py
+++ src/generators.py
@@ -2,6 +2,7 @@ from typing import Iterator, Generator
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
+    """Принимает список трзнакций и возвращает их те, которые производились с определенной валютой"""
     if not transactions:
         yield "Список не должен быть пустым"
     for transaction in transactions:
@@ -12,6 +13,7 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """Принимает список трзнакций и возвращает их описания"""
     if not transactions:
         yield "Список не должен быть пустым"
     for transaction in transactions:
@@ -19,6 +21,7 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
 
 
 def card_number_generator(start_number: int, end_number: int) -> Generator[str, None, None]:
+    """Генерирует номера карт от начального номера до конечно(от start_number до end_number соответственно)"""
     for i in range(start_number, end_number + 1):
         card_number = str(i).zfill(16)
         formatted_card_number = " ".join([card_number[j:j+4] for j in range(0, 16, 4)])
